0f8a82e8b842234b23c0ef6166de5b269c5fb151
Sofia <jonssonsofia93@gmail.com>
2019-10-17 12:35:15 -0700

    sum of two integers

---
+++ Easy/Sum of Two Integers.rb
@@ -0,0 +1,18 @@
+#
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+#
+# Example 1:
+#
+# Input: a = 1, b = 2
+# Output: 3
+# Example 2:
+#
+# Input: a = -2, b = 3
+# Output: 1
+
+# @param {Integer} a
+# @param {Integer} b
+# @return {Integer}
+def get_sum(a, b)
+     result= [a,b].sum
+end
